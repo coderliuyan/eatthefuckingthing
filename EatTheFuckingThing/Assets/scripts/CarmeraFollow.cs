afb0b2483dcded54a69327ade4e74d807f5f70c2
刘岩 <362504741@qq.com>
2018-10-23 11:10:18 +0800

    1.增加了camera follow的脚本,让相机跟随玩家,设定左右的范围,让玩家不会看到边缘之外

---
+++ EatTheFuckingThing/Assets/scripts/CarmeraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarmeraFollow : MonoBehaviour {
+
+    public Transform playerTranform;
+	// Use this for initialization
+	void Start () {
+        playerTranform = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(playerTranform.position.x < 490f && playerTranform.position.x > 75f)
+        {
+            transform.position = new Vector3(playerTranform.position.x, playerTranform.position.y, transform.position.z);
+        }
+
+    }
+}
